66738c41b816f3a2360aa838c568e4d74e5243c5
marinagosson <marina.gosson@gmail.com>
2016-05-04 19:25:44 -0300

    Tela de chats.

--- app/src/main/java/br/com/meetstudents/ChatActivity.java
+++ app/src/main/java/br/com/meetstudents/ChatActivity.java
@@ -1,22 +1,19 @@
 package br.com.meetstudents;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
-
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_chat);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_toolbar_register);
-        toolbar.setTitle(R.string.title_activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_activity_chat);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -31,12 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
             return true;
         }
 
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 
-    public void onClickRegistar(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/br/com/meetstudents/MainActivity.java
+++ app/src/main/java/br/com/meetstudents/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.meetstudents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,27 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (item.getItemId() == R.id.action_chat) {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
